fa5891cbb60e3434143761c74b92c6ade4a81ca4
ryoshi-noname <ryoshi100@gmail.com>
2025-01-04 13:33:12 -0800

    Updated settings in CSS file for point size

--- src/main/java/roy/batterydata/DataViewer.java
+++ src/main/java/roy/batterydata/DataViewer.java
@@ -1,12 +1,17 @@
 package roy.batterydata;
 
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -54,12 +59,24 @@ public class DataViewer {
 
             lineChart.getData().add(series);
 
+//            Node line = series.getNode();
+//            String color = line.getStyle().replaceAll(".*-fx-stroke:\\s*([^;]+);.*", "$1");
+//            Set<Node> legendItem = lineChart.lookupAll(".chart-legend-item");
+//            Label legendLabel = (Label) legendItem.lookup(".chart-legend-item-symbol + Label");
+////                            legendLabel.setStyle("-fx-text-fill: " + color + ";");
         }
 
         Scene scene = new Scene(lineChart, 1080, 640);
-        // Line below lets the javaFX scene use the "styles.css" file, located in resources of the project
+
+        // Code below lets the javaFX scene use the "styles.css" file, located in resources of the project
         // to modify elements (called nodes in javaFX) in the scene
-        scene.getStylesheets().add(DataViewer.class.getResource("styles.css").toExternalForm());
+        URL cssUrl = DataViewer.class.getResource("styles.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        } else {
+            System.err.println("Stylesheet not found!");
+        }
+
         return scene;
 
     }
